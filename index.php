e466032c477fcd690e249ce3b57fd500fd8be35e
benito6z <modica.b@outlook.fr>
2022-05-02 14:36:13 +0200

    form items HTML

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
     <title>netfalse</title>
 </head>
 <body>
-    
+    <form method="POST">
+
+    <label>Titre:</label><br>
+    <input type="text" name="titre"><br>
+
+    <label>Déscription:</label><br>
+    <textarea name="description">
+            It was a dark and stormy night...
+    </textarea><br>
+
+    <label>Duration:</label><br>
+    <input type="text" name="duration"><br>
+
+    <label for="file">Fichier</label>
+    <input type="file" name="file"><br>
+
+    <input type="checkbox" id="categorie1" name="categorie1" value="Action">
+    <label for="categorie1">Action</label><br>
+    <input type="checkbox" id="categorie2" name="categorie2" value="Histoire">
+    <label for="categorie2">Histoire</label><br>
+    <input type="checkbox" id="categorie3" name="categorie3" value="Animé">
+    <label for="categorie3">Animé</label><br>
+    <input type="checkbox" id="categorie4" name="categorie4" value="Syfy">
+    <label for="categorie4">Syfy</label>
+    </form>
 </body>
 </html>
